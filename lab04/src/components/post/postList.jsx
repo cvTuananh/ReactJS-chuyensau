00407a6043e179c7d1b3f70fa48c05588ee46257
TuanAnhViCu <anh.cu@madison-technogoly.com>
2022-01-16 17:51:22 +0700

    form

--- lab04/src/components/post/postList.jsx
+++ lab04/src/components/post/postList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import './style.css'
 import {useSelector} from 'react-redux'
 
 export const PostList = ()=>{
--- lab04/src/pages/index.jsx
+++ lab04/src/pages/index.jsx
@@ -1,12 +1,42 @@
+import './style.css'
 import { PostList } from "../components/post/postList";
+import {
+    BrowserRouter as Router,
+    Link,
+    Route,
+    Routes,
+    Navigate
+}from 'react-router-dom';
+import {NavBar} from '../components/navBar'
+import './style.css'
+import React from 'react';
+import { PostForm } from '../components/post/postForm';
 
 function Index() {
     return ( 
-        <div>
-            <h1>Welcome to </h1>
-            <PostList />
-        </div>
+        <Router>
+            <h1>Welcome to blog Application </h1>
+            <NavBar />
+              <Routes>
+                  {/* <Route exact path ='/' element={<Home />}/> */}
+                  <Route exact path ="/" element={(
+                       <React.Fragment>
+                       <PostForm/>
+                       <PostList/>
+                   </React.Fragment>
+                  )}/>
+              </Routes>
+              <Navigate to="/" />
+        </Router>
      );
 }
 
 export default Index;
+
+const Home = (props) => {
+    return(
+        <div>
+            <h1>Welcome to Blog application</h1>
+        </div>
+    )
+}
